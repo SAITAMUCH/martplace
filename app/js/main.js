66c4622e0fcd5d06ad80e8eb411c8d035ee2fe74
SAITAMUCH <yakovchuk.anton.ua@gmail.com>
2020-04-14 20:04:20 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
-$(function(){
+$(function () {
+
+    $(".slider__inner").slick({
+        dots: false,
+        prevArrow: '<div class="prev"></div>',
+        nextArrow: '<div class="next"></div>',
+    });
+
+    $(".rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "17px",
+        readOnly: true,
+        normalFill: "grey"
+    });
+
 
     
+    var mixer = mixitup('.products__inner-box');
 
 });
+

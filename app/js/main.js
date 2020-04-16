daa9dd04ceab4f275c891f53e89bbf047aef5268
SAITAMUCH <yakovchuk.anton.ua@gmail.com>
2020-04-16 13:24:50 +0300

    added products

--- app/js/main.js
+++ app/js/main.js
@@ -2,19 +2,20 @@ $(function () {
 
     $(".slider__inner").slick({
         dots: false,
+        draggable: false,
         prevArrow: '<div class="prev"></div>',
         nextArrow: '<div class="next"></div>',
     });
 
     $(".rate-star").rateYo({
         rating: 4.5,
-        starWidth: "17px",
+        starWidth: "15px",
         readOnly: true,
         normalFill: "grey"
     });
 
 
-    
+
     var mixer = mixitup('.products__inner-box');
 
 });
